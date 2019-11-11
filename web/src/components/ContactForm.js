ed69fed370bc2b58382a7bffd399ea702dad858b
Thor Christian Nilsen <thorchrelind@gmail.com>
2019-11-11 15:37:10 +0100

    Adding success.js

--- web/src/components/ContactForm.js
+++ web/src/components/ContactForm.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 const ContactForm = () => (
   <div>
-    <form name="contact" 
-      method="post" 
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      >
-      <input name="name" placeholde="Ditt navn" type="text" />
-      <button></button>
-      
-    </form>   
+    <form name='contact'
+      method='post'
+      action='/success'
+      data-netlify='true'
+      data-netlify-honeypot='bot-field'
+    >
+      <input name='name' placeholde='Ditt navn' type='text' />
+      <button />
+
+    </form>
   </div>
 )
 
