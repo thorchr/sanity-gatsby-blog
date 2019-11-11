8b0783e722ca4f0e9c7bb040d8c226b1450ab553
Thor Christian Nilsen <thorchrelind@gmail.com>
2019-11-11 16:06:10 +0100

    Update success.js

--- web/src/pages/success.js
+++ web/src/pages/success.js
@@ -1,8 +1,14 @@
-import React from "react"
+import React from 'react'
+import Container from '../components/container'
+import Layout from '../containers/layout'
 
 export default () => (
-  <div>
+  <Layout>
+    <Container>
+      <h2>You dropped me a new line</h2>
+      <h4> Congrats,i&#39;ll be sure to check it out when i can and add a reply</h4>
+    </Container>
     <h2>You dropped me a new line</h2>
     <h4> Congrats,i&#39;ll be sure to check it out when i can and add a reply</h4>
-  </div>
+  </Layout>
 )
