a5b8317e24b0c5959cd84b81ce28fd2dcc31b683
Thor Christian Nilsen <thorchrelind@gmail.com>
2019-11-11 16:25:11 +0100

    Update success.js

--- web/src/pages/success.js
+++ web/src/pages/success.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Container from '../components/container'
 import Layout from '../containers/layout'
+import Link from 'gatsby-link'
 
 export default () => (
   <Layout>
     <Container>
       <h2>You dropped me a new line</h2>
       <h4> Congrats,i&#39;ll be sure to check it out when i can and add a reply</h4>
+      <Link to='/'>Til forsiden</Link>
     </Container>
-    <h2>You dropped me a new line</h2>
-    <h4> Congrats,i&#39;ll be sure to check it out when i can and add a reply</h4>
+    
   </Layout>
 )
