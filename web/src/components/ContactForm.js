eb2a25baa681b0455c5796fd9be50b3d23fd7eb0
Thor Christian Nilsen <thorchrelind@gmail.com>
2019-11-11 15:28:34 +0100

    Update ContactForm.js

--- web/src/components/ContactForm.js
+++ web/src/components/ContactForm.js
@@ -2,21 +2,15 @@ import React from 'react'
 
 const ContactForm = () => (
   <div>
-    <form name='contact' method='post' data-netlify-honeypot='bot-field' data-netlify='true'>
-      <input type='hidden' name='bot-field' />
-      <p>
-        <label>Your Name: <input type='text' name='name' /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type='email' name='email' /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name='message' /></label>
-      </p>
-      <p>
-        <button type='submit'>Send</button>
-      </p>
-    </form>
+    <form name="contact" 
+      method="post" 
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      >
+      <input name="name" placeholde="Ditt navn" type="text" />
+      <button></button>
+      
+    </form>   
   </div>
 )
 
