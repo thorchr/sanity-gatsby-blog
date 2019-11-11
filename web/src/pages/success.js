a2f4fffeead17215a54c2aa889156b18ab0f8d76
Thor Christian Nilsen <thorchrelind@gmail.com>
2019-11-11 16:33:17 +0100

    Update success.js

--- web/src/pages/success.js
+++ web/src/pages/success.js
@@ -6,8 +6,8 @@ import Link from 'gatsby-link'
 export default () => (
   <Layout>
     <Container>
-      <h2>You dropped me a new line</h2>
-      <h4> Congrats,i&#39;ll be sure to check it out when i can and add a reply</h4>
+      <h2>Beskjeden er sendt til styret</h2>
+      <h4> En fra styret vil ta kontakt med deg</h4>
       <Link to='/'>Til forsiden</Link>
     </Container>
     
